6663f59b001374a8a705c1119b00cafc8edbbd9e
santanu12saha <saha.santanu0217@gmail.com>
2020-08-30 18:13:46 +0530

    Graph Prod Configuration process.

--- config/mongo.js
+++ config/mongo.js
@@ -2,5 +2,9 @@ module.exports = {
     development: {
       url: 'mongodb://localhost:27017/',
       database: 'contests'
+    },
+    production: {
+      url: process.env.MONGODB_URI,
+      database: process.env.MONGODB_DATABASE_NAME
     }
 };
--- config/pg.js
+++ config/pg.js
@@ -2,5 +2,12 @@
 module.exports = {
     development: {
       database: 'contests'
+    },
+    production: {
+      database: process.env.PG_DATABASE_NAME,
+      user: process.env.PG_DATABASE_USERNAME,
+      host: process.env.PG_DATABASE_HOST,
+      password: process.env.PG_DATABASE_PASSWORD,
+      port: process.env.PG_DATABASE_PORT
     }
 };
